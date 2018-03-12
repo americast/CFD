02f58e058850f038b5377ed4099f0a47ac12b0f4
vernwalrahul <vernwalrahul@gmail.com>
2018-03-12 21:26:43 +0530

    updated

--- map/app.py
+++ map/app.py
@@ -56,7 +56,8 @@ def traffic_new():
     flash("Done!")
  
 if __name__ == "__main__":
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.debug = True
-    app.run(host= '0.0.0.0', port=80)
+    app.run()
+    # app.secret_key = 'super secret key'
+    # app.config['SESSION_TYPE'] = 'filesystem'
+    # app.debug = True
+    # app.run(host= '0.0.0.0', port=80)
